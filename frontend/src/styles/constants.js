0f68fb903edc00a5ce0c59f2125aa04f7653d85b
Eva <e037l862712@gmail.com>
2024-03-19 00:24:26 +0800

    chore: add color pattern

---
+++ frontend/src/styles/constants.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  'primary-600': '#3B2826',
+  'primary-500': '#62251D',
+  'primary-400': '#875D50',
+  'primary-300': '#836E6E',
+  'primary-200': '#BBAFA2',
+  'primary-100': '#F1E3D5',
+  red: '#D1104A',
+};
