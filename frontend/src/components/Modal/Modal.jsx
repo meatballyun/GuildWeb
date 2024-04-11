0423425157be1a2e9347763e487d282a3f2c8c82
Eva <e037l862712@gmail.com>
2024-04-11 23:14:56 +0800

    feat: add Modal footButton

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,22 +1,27 @@
 import { MaterialSymbol } from '../MaterialSymbol';
 import './styles.css';
-export const Modal = ({ isOpen, onClose, header, children }) => {
+export const Modal = ({ isOpen, onClose, header, children, footButton }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal-container">
         <div className="modal">
-          <div className="flex w-full items-center justify-between p-2 text-center">
-            <span></span>
+          <div className="flex w-full items-center justify-between border-b-2 border-primary-200/30 p-2 text-center">
+            <span />
             <span className="text-heading-h2">{header}</span>
             <MaterialSymbol
               className="cursor-pointer"
               icon="close"
-              onClick={() => onClose()}
+              onClick={() => onClose?.()}
             />
           </div>
           <div className="modal-content">{children}</div>
+          {footButton && (
+            <div className="border-t-2 border-primary-200/30 p-2">
+              {footButton}
+            </div>
+          )}
         </div>
       </div>
     </div>
