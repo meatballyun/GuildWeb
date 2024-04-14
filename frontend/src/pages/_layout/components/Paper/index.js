34b6e83a2db0a79e083b9e2d6f8a71a3c13a98f8
Eva <e037l862712@gmail.com>
2024-04-15 04:29:03 +0800

    fix: fix layout paper component

--- frontend/src/pages/_layout/components/Paper/index.js
+++ frontend/src/pages/_layout/components/Paper/index.js
@@ -1,16 +1,1 @@
-import { classNames } from '../../../../utils';
-import './styles.css';
-export const Paper = ({ children, className, row, ...props }) => {
-  return (
-    <div
-      {...props}
-      className={classNames(
-        'layout-paper',
-        row && 'layout-paper--row',
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+export * from './Paper';
