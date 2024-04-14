d096816c5aec80814aa66b222ddc2dbcb835ad89
Eva <e037l862712@gmail.com>
2024-04-15 04:24:56 +0800

    fix: fix input element

--- frontend/src/components/Form/TextArea.jsx
+++ frontend/src/components/Form/TextArea.jsx
@@ -1,3 +1,24 @@
-export const TextArea = ({ onChange, ...props }) => {
-  return <textarea {...props} onChange={(e) => onChange(e.target.value)} />;
+import { classNames } from '../../utils';
+import './styles.css';
+
+export const TextArea = ({
+  onChange,
+  noFill,
+  disabled,
+  className,
+  error,
+  ...props
+}) => {
+  return (
+    <textarea
+      className={classNames(
+        'input_container textArea_container text-paragraph-p3',
+        !disabled && !noFill && 'bg-primary-100',
+        className
+      )}
+      disabled={disabled}
+      {...props}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
 };
