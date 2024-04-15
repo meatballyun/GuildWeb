79783ab9a3f64186638006cc27e9f46bfe8e781c
Rex <rex.rex022534@gmail.com>
2024-04-15 21:46:39 +0800

    feat: add send friend invitation

---
+++ backend/models/userFriendModel.js
@@ -0,0 +1,42 @@
+const connection = require('../lib/db');
+
+class UserFriendModel {
+  static addFriend(USER_ID, FRIEND_ID) {
+		return new Promise((resolve, reject) => {
+			connection.query('INSERT INTO userFriends (USER_ID, FRIEND_ID) VALUES (?,?)', [USER_ID, FRIEND_ID], function (err, rows) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
+		});
+	}
+
+  static getFriend(USER_ID, STATUS) {
+		return new Promise((resolve, reject) => {
+			connection.query('SELECT FRIEND_ID FROM userFriends WHERE USER_ID = ? AND STATUS = ?', [USER_ID, STATUS], function (err, rows) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
+		});
+	}
+
+  static updateFriend(USER_ID, FRIEND_ID, STATUS) {
+		return new Promise((resolve, reject) => {
+			connection.query('UPDATE userFriends SET STATUS = ? WHERE USER_ID = ? AND FRIEND_ID = ?', [STATUS, USER_ID, FRIEND_ID], function (err, rows) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
+		});
+	}
+
+};
+
+module.exports = UserFriendModel;
