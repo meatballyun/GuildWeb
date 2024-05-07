0df4ad3ef71af2b8d978107c7b02b5452bb772c6
Rex <rex.rex022534@gmail.com>
2024-05-07 21:59:25 +0800

    fix: front setproxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,7 @@
 // src/setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+console.log(`${process.env.REACT_APP_BE_URL}:${process.env.REACT_APP_BE_PORT}`);
 module.exports = function (app) {
   app.use(
     '/api',
