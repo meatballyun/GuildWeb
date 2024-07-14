0ac0abc382c7847c0be0bddf5e27d90ef08fd206
Rex <rex.rex022534@gmail.com>
2024-07-14 22:56:33 +0800

    [refactor] missionTemplateItemService: put query in transcation

--- backend/src/services/guild/missionTemplate.ts
+++ backend/src/services/guild/missionTemplate.ts
@@ -6,6 +6,7 @@ import { MissionTemplateInfo, MissionTemplateTime } from '../../types/guild/miss
 import { Item } from './missionTemplateItem';
 import { UserModel, MissionTemplateModel, MissionTemplateItemModel } from '../../models';
 import * as missionTemplateItemService from './missionTemplateItem';
+import { executeTransaction } from '../../utils/executeTransaction';
 
 interface MissionTemplateDetailed extends MissionTemplateTime, MissionTemplateInfo {
   items: Item[];
@@ -38,12 +39,15 @@ export const getOne = async (missionTemplateId: number) => {
 };
 
 export const create = async ({ generationTime, deadline, items, ...otherData }: MissionTemplateDetailed, guildId: number, uid: number) => {
-  let time = await timeHandle(generationTime, deadline);
-  const newTemplateId = await MissionTemplateModel.create(uid, guildId, time, otherData);
-  if (!newTemplateId) throw new ApplicationError(400);
+  let time = timeHandle(generationTime, deadline);
+  const id = await executeTransaction(async () => {
+    const newTemplateId = await MissionTemplateModel.create(uid, guildId, time, otherData);
+    if (!newTemplateId) throw new ApplicationError(400);
 
-  await missionTemplateItemService.create(items, newTemplateId);
-  return { id: newTemplateId };
+    await missionTemplateItemService.create(items, newTemplateId);
+    return newTemplateId;
+  });
+  return id;
 };
 
 export const update = async ({ generationTime, deadline, items, ...otherData }: MissionTemplateDetailed, missionTemplateId: number, membership: Membership, uid: number) => {
@@ -51,33 +55,34 @@ export const update = async ({ generationTime, deadline, items, ...otherData }: 
   if (!missionTemplate) throw new ApplicationError(404);
   if (membership === 'vice' && uid !== missionTemplate.creatorId) throw new ApplicationError(403);
   if (generationTime > deadline) throw new ApplicationError(409);
+  executeTransaction(async () => {
+    const time = { generationTime: formatTimestamp(generationTime), deadline: formatTimestamp(deadline) };
+    const result = await MissionTemplateModel.update(missionTemplateId, time, otherData);
+    if (!result) throw new ApplicationError(400);
 
-  const time = { generationTime: formatTimestamp(generationTime), deadline: formatTimestamp(deadline) };
-  const result = await MissionTemplateModel.update(missionTemplateId, time, otherData);
-  if (!result) throw new ApplicationError(400);
-
-  const templateItems: { id: number; content: string }[] = [];
-  const newContents: string[] = [];
-  const deleteIds: number[] = [];
-  if (items) {
-    items.map(({ id, content }) => {
-      if (content) {
-        id ? templateItems.push({ id, content }) : newContents.push(content);
-      } else deleteIds.push(id);
-    });
-    if (templateItems?.length) await MissionTemplateItemModel.updateMany(templateItems);
-    if (newContents?.length) await MissionTemplateItemModel.createMany(missionTemplateId, newContents);
-    if (deleteIds?.length) await MissionTemplateItemModel.deleteManyById(deleteIds);
-  } else await missionTemplateItemService.remove(missionTemplateId);
+    const templateItems: { id: number; content: string }[] = [];
+    const newContents: string[] = [];
+    const deleteIds: number[] = [];
+    if (items) {
+      items.map(({ id, content }) => {
+        if (content) {
+          id ? templateItems.push({ id, content }) : newContents.push(content);
+        } else deleteIds.push(id);
+      });
+      if (templateItems?.length) await MissionTemplateItemModel.updateMany(templateItems);
+      if (newContents?.length) await MissionTemplateItemModel.createMany(missionTemplateId, newContents);
+      if (deleteIds?.length) await MissionTemplateItemModel.deleteManyById(deleteIds);
+    } else await missionTemplateItemService.remove(missionTemplateId);
+  });
 };
 
 export const remove = async (missionTemplateId: number, membership: Membership, uid: number) => {
   const missionTemplate = await MissionTemplateModel.getOne(missionTemplateId);
   if (!missionTemplate) throw new ApplicationError(404);
   if (membership === 'vice' && uid !== missionTemplate.creatorId) throw new ApplicationError(403);
-
-  await missionTemplateItemService.remove(missionTemplateId);
-
-  const deleteMissionTemplate = await MissionTemplateItemModel.delete(missionTemplateId);
-  if (!deleteMissionTemplate) throw new ApplicationError(400);
+  executeTransaction(async () => {
+    await missionTemplateItemService.remove(missionTemplateId);
+    const deleteMissionTemplate = await MissionTemplateItemModel.delete(missionTemplateId);
+    if (!deleteMissionTemplate) throw new ApplicationError(400);
+  });
 };
