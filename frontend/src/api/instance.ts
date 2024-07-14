6d911a3c23adb438382b9531a17affac51c8a1b9
Eva <e037l862712@gmail.com>
2024-07-14 23:29:51 +0800

    fix: fix errors

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { BASE_API_URL } from './constants';
 
 export const baseInstance = axios.create({
-  baseURL: BASE_API_URL,
+  baseURL: './api',
 });
--- frontend/src/setupProxy.ts
+++ frontend/src/setupProxy.ts
@@ -1,13 +1,16 @@
-// src/setupProxy.js
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import { Application } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 console.log(`${process.env.REACT_APP_BE_URL}:${process.env.REACT_APP_BE_PORT}`);
-module.exports = function (app) {
+
+const setupProxy = (app: Application) => {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: `${process.env.REACT_APP_BE_URL}:${process.env.REACT_APP_BE_PORT}`, // Change this to your backend server URL
+      target: `${process.env.REACT_APP_BE_URL}:${process.env.REACT_APP_BE_PORT}`,
       changeOrigin: true,
     })
   );
 };
+
+export default setupProxy;
