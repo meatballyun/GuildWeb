2ed5e84ff94c350700382d8ac14129664ff9c28a
Eva <e037l862712@gmail.com>
2024-07-17 07:55:10 +0800

    fix(fe): api add token

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const token = localStorage.token;
+
 export const baseInstance = axios.create({
   baseURL: './api',
+  headers: {
+    Authorization: token && `Bearer ${token}`,
+  },
 });
